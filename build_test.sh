6e43757e850280094f3e85a93c090f37d03b4429
Douglas Beal <public.git@dougbeal.com>
2019-02-07 12:19:58 -0800

    query for all push rules, then run them

--- build_test.sh
+++ build_test.sh
@@ -8,5 +8,9 @@ echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 # Bazel build and test
 bazel build --curses=no -s --verbose_explanations --verbose_failures //...
 bazel test --curses=no --test_output=errors //...
-bazel run //...
+for push in $(bazel query 'filter("push*", kind("rule", //...))')
+do
+    bazel run $push
+done
+
 
