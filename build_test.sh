bd5442086aa54a7f926493fe7e732c0a383a0ddc
Douglas Beal <public.git@dougbeal.com>
2019-02-06 19:59:45 -0800

    use docker login to enerate token

--- build_test.sh
+++ build_test.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 set -xe
 
-# docker credentials
-echo """
-{
-	"ServerURL": "https://index.docker.io/v1",
-	"Username": "${DOCKER_USERNAME}",
-	"Secret": "${DOCKER_PASSWORD}"
-}
-""" > ./config.json
+# docker credential
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+
 # Bazel build and test
 bazel build --curses=no -s --verbose_explanations --verbose_failures //...
 bazel test --curses=no --test_output=errors //...
